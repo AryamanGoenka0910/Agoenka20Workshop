672c78a6c4d4623795149edf6f0d76bedb5f59e7
Aryaman Goenka <arygoenka@gmail.com>
2021-09-28 12:51:14 -0400

    Comments addeed

--- 07_py-csv/occupation.py
+++ 07_py-csv/occupation.py
@@ -1,5 +1,25 @@
+##
+# Elon Musk: Haotian Gan, Aryaman Goenka, Tomas Acuna (Interm Member While Sean is Out)  
+# Soft Dev
+#
+# K06 StI/O: Divine your Destiny!/Python Refresher/Write a Python script to read the file and build an appropriate dictionary from it.
+# Create a function that returns a randomly selected occupation where the results are weighted by the percentage given.
+#
+# 2021/09/28
+#
+#
+##
+
+# SUMMARY OF TRIO DISCUSSION
+# We First Discussed as a group the best way to tackle this problem 
+# We then divided the work and began solving the problem to the best of our abilites
+
+# DISCOVERIES
+# It is a good thing to note that you should never mutate the data unless it is extremely necessary. 
+
 import csv, random
 
+#Function to read csv file and transfer it to an approriate dictionary
 def readfile():
     file = open("occupations.csv")
     csvreader = csv.reader(file)
@@ -11,7 +31,7 @@ def readfile():
     file.close()
     return occupations
 
-
+#Using the weights provided in percentages, generate a randomly selected occupation
 def genorateRandom():
     occupations = readfile()
     ran = random.randint(1, 100)
@@ -22,7 +42,7 @@ def genorateRandom():
             return row
     return
 
-
+#Run functions and print an output
 def main():
     print(genorateRandom())
 
